4464566e4c5968fa6dcc34b6318ef8f39aaab9c1
Matt <ukyg9e5r6k7gubiekd6@yahoo.com>
2013-11-01 20:29:35 +1100

    Initial import: First pass at a C client for Swift using libcurl and iconv.

---
+++ swift-client.c
@@ -0,0 +1,254 @@
+#include <assert.h>
+#include <string.h>
+#include <iconv.h>
+#include <errno.h>
+
+#include "swift-client.h"
+
+/**
+ * The maximum length in bytes of a multi-byte UTF-8 sequence.
+ *
+ */
+#define UTF8_SEQUENCE_MAXLEN 6
+
+/**
+ * Default handler for curl errors.
+ */
+static void
+default_curl_error_callback(const char *curl_funcname, CURLcode curl_err)
+{
+	assert(curl_funcname != NULL);
+	fprintf(stderr, "%s failed: libcurl error code %ld: %s\n", curl_funcname, (long) curl_err, curl_easy_strerror(curl_err));
+}
+
+static void
+default_iconv_error_callback(const char *iconv_funcname, int iconv_errno)
+{
+	assert(iconv_funcname != NULL);
+	errno = iconv_errno;
+	perror(iconv_funcname);
+}
+
+static void *
+default_allocator(size_t size)
+{
+	return malloc(size);
+}
+
+static void *
+default_reallocator(void *ptr, size_t newsize)
+{
+	return realloc(ptr, newsize);
+}
+
+static void
+default_deallocator(void *ptr)
+{
+	free(ptr);
+}
+
+enum swift_error
+swift_global_init(void)
+{
+	CURLcode curl_err;
+
+	curl_err = curl_global_init(CURL_GLOBAL_ALL);
+	if (curl_err != 0) {
+		/* TODO: Output error indications about detected error in 'res' */
+		return SCERR_INIT_FAILED;
+	}
+	return SCERR_SUCCESS;
+}
+
+void
+swift_global_cleanup(void)
+{
+	curl_global_cleanup();
+}
+
+enum swift_error
+swift_start(swift_context_t *context)
+{
+	assert(context != NULL);
+	if (!context->curl_error) {
+		context->curl_error = default_curl_error_callback;
+	}
+	if (!context->iconv_error) {
+		context->iconv_error = default_iconv_error_callback;
+	}
+	if (!context->allocator) {
+		context->allocator = default_allocator;
+	}
+	if (!context->reallocator) {
+		context->reallocator = default_reallocator;
+	}
+	if (!context->deallocator) {
+		context->deallocator = default_deallocator;
+	}
+	context->pvt.iconv = iconv_open("WCHAR_T", "UTF-8");
+	if ((iconv_t) -1 == context->pvt.iconv) {
+		context->iconv_error("iconv_open", errno);
+		return SCERR_INIT_FAILED;
+	}
+	context->pvt.curl = curl_easy_init();
+	if (NULL == context->pvt.curl) {
+		/* NOTE: No error code from libcurl, so we assume/invent CURLE_FAILED_INIT */
+		context->curl_error("curl_easy_init", CURLE_FAILED_INIT);
+		return SCERR_INIT_FAILED;
+	}
+	return SCERR_SUCCESS;
+}
+
+void
+swift_end(swift_context_t **context)
+{
+	assert(context != NULL);
+	assert(*context != NULL);
+	curl_easy_cleanup((*context)->pvt.curl);
+	(*context)->pvt.curl = NULL;
+	if ((*context)->pvt.url != NULL) {
+		(*context)->deallocator((*context)->pvt.url);
+		(*context)->pvt.url = NULL;
+	}
+	if ((*context)->pvt.hostname != NULL) {
+		(*context)->deallocator((*context)->pvt.hostname);
+		(*context)->pvt.hostname = NULL;
+	}
+	if ((*context)->pvt.container != NULL) {
+		(*context)->deallocator((*context)->pvt.container);
+		(*context)->pvt.container = NULL;
+	}
+	if ((*context)->pvt.object != NULL) {
+		(*context)->deallocator((*context)->pvt.object);
+		(*context)->pvt.object = NULL;
+	}
+	if (iconv_close((*context)->pvt.iconv) < 0) {
+		(*context)->iconv_error("iconv_close", errno);
+	}
+	*context = NULL;
+}
+
+static enum swift_error
+utf8_and_url_encode(swift_context_t *context, const wchar_t *in, char *out)
+{
+	char *url_encoded, *iconv_in, *iconv_out;
+	size_t in_len, utf8_in_len, iconv_in_len, iconv_out_len;
+
+	assert(context != NULL);
+	assert(in != NULL);
+	assert(out != NULL);
+	/* Convert the wchar_t input to UTF-8 and write the result to out */
+	in_len = wcslen(in);
+	utf8_in_len = in_len * UTF8_SEQUENCE_MAXLEN; /* Assuming worst-case UTF-8 expansion */
+	context->reallocator(out, utf8_in_len);
+	if (NULL == out) {
+		return SCERR_ALLOC_FAILED;
+	}
+	iconv_in_len = in_len * sizeof(wchar_t); /* iconv counts in bytes not chars */
+	iconv_out_len = utf8_in_len;
+	iconv_in = (char *) in;
+	iconv_out = out;
+	if ((size_t) -1 == iconv(context->pvt.iconv, &iconv_in, &iconv_in_len, &iconv_out, &iconv_out_len)) {
+		/* This should be impossible, as all wchar_t values should be expressible in UTF-8 */
+		context->iconv_error("iconv", errno);
+		return SCERR_INVARG;
+	}
+	/* Create a URL-encoded copy of out in memory newly-allocated by libcurl */
+	url_encoded = curl_easy_escape(context->pvt.curl, out, in_len);
+	if (NULL == url_encoded) {
+		return SCERR_ALLOC_FAILED;
+	}
+	/* Copy the URL-encoded value into out, over-writing its previous UTF-8 value */
+	context->reallocator(out, strlen(url_encoded) + 1 /* '\0' */);
+	if (NULL == out) {
+		return SCERR_ALLOC_FAILED;
+	}
+	strcpy(out, url_encoded);
+	/* Free the URL-encoded copy created by libcurl */
+	curl_free(url_encoded);
+
+	return SCERR_SUCCESS;
+}
+
+enum swift_error
+swift_set_container(swift_context_t *context, wchar_t *container_name)
+{
+	return utf8_and_url_encode(context, container_name, context->pvt.container);
+}
+
+enum swift_error
+swift_set_object(swift_context_t *context, wchar_t *object_name)
+{
+	return utf8_and_url_encode(context, object_name, context->pvt.object);
+}
+
+static enum swift_error
+make_url(swift_context_t *context)
+{
+	assert(context != NULL);
+	assert(context->pvt.hostname != NULL);
+	assert(context->pvt.container != NULL);
+	assert(context->pvt.object != NULL);
+	context->pvt.url = context->reallocator(
+		context->pvt.url,
+		4 /* "http" */
+		+ (context->pvt.ssl ? 0 : 1) /* 's'? */
+		+ 3 /* "://" */
+		+ strlen(context->pvt.hostname)
+		+ 1 /* '/' */
+		+ strlen(context->pvt.container)
+		+ 1 /* '/' */
+		+ strlen(context->pvt.object)
+		+ 1 /* '\0' */
+	);
+	if (NULL == context->pvt.url) {
+		return SCERR_ALLOC_FAILED;
+	}
+	sprintf(
+		context->pvt.url,
+		"http%s://%s/%s/%s",
+		(context->pvt.ssl ? "s" : ""),
+		context->pvt.hostname,
+		context->pvt.container,
+		context->pvt.object
+	);
+	return SCERR_SUCCESS;
+}
+
+enum swift_error
+swift_get(swift_context_t *context)
+{
+	/* TODO */
+	CURLcode curl_err;
+	enum swift_error sc_err;
+
+	assert(context != NULL);
+	sc_err = make_url(context);
+	if (sc_err != SCERR_SUCCESS) {
+		return sc_err;
+	}
+	curl_easy_setopt(context->pvt.curl, CURLOPT_URL, context->pvt.url);
+	/* TODO: curl_easy_setopt method */
+	curl_err = curl_easy_perform(context->pvt.curl);
+	if (CURLE_OK != curl_err) {
+		context->curl_error("curl_easy_perform", curl_err);
+		return SCERR_URL_FAILED;
+	}
+	return SCERR_SUCCESS;
+}
+
+enum swift_error
+swift_put(swift_context_t *context)
+{
+	assert(context != NULL);
+	/* TODO */
+	return SCERR_SUCCESS;
+}
+
+enum swift_error
+swift_post(swift_context_t *context, wchar_t *name, wchar_t *value)
+{
+	assert(context != NULL);
+	/* TODO */
+	return SCERR_SUCCESS;
+}
---
+++ swift-client.h
@@ -0,0 +1,156 @@
+#ifndef SWIFT_CLIENT_H_
+#define SWIFT_CLIENT_H_
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <malloc.h>
+#include <wchar.h>
+#include <curl/curl.h>
+
+/**
+ * High-level types of errors which can occur while attempting to use Swift.
+ * More detail is available from lower-level libraries (such as curl)
+ * using error callbacks specific to those libraries.
+ */
+enum swift_error {
+	SCERR_SUCCESS      = 0, /* Success */
+	SCERR_INIT_FAILED  = 1, /* Initialisation of this library failed */
+	SCERR_INVARG       = 2, /* An invalid argument was supplied */
+	SCERR_ALLOC_FAILED = 3, /* memory allocation failed */
+	SCERR_URL_FAILED   = 4  /* network operation on a URL failed */
+};
+
+/* A function which allocates memory */
+typedef void *(*swift_allocator_func_t)(size_t size);
+
+/* A function which re-allocates memory */
+typedef void *(*swift_reallocator_func_t)(void *ptr, size_t newsize);
+
+/* A function which de-allocates memory */
+typedef void (*swift_deallocator_func_t)(void *ptr);
+
+/* A function which receives curl errors */
+typedef void (*curl_error_callback_t)(const char *curl_funcname, CURLcode res);
+
+/* A function which receives libiconv errors */
+typedef void (*iconv_error_callback_t)(const char *iconv_funcname, int iconv_errno);
+
+/* swift client library's per-thread private context */
+struct swift_context_private {
+	CURL *curl;       /* Handle to curl library's easy interface */
+	iconv_t iconv;    /* iconv library's conversion descriptor */
+	char *hostname;   /* hostname or dotted-decimal IP of Swift server */
+	unsigned int ssl; /* True if SSL in use, false otherwise */
+	char *container;  /* Name of current container */
+	char *object;     /* Name of current object */
+	char *url;        /* The URL currently being used */
+};
+
+typedef struct swift_context_private swift_context_private_t;
+
+/**
+ * All use of this library is performed within a 'context'.
+ * Contexts cannot be shared among threads; each thread must have its own context.
+ * Your program is responsible for allocating and freeing context structures.
+ * Contexts should be zeroed out prior to use.
+ */
+struct swift_context {
+
+	/* These members are 'public'; your program can (and should) set them at will */
+
+	/**
+	 * Called when a libcurl error occurs.
+	 * Your program may set this function pointer in order to perform custom error handling.
+	 * If this is NULL at the time swift_start is called, a default handler will be used.
+	 */
+	curl_error_callback_t curl_error;
+	/**
+	 * Called when a libiconv error occurs.
+	 * Your program may set this function in order to perform custom error handling.
+	 * If this is NULL at the time swift_start is called, a default handler will be used.
+	 */
+	iconv_error_callback_t iconv_error;
+	/**
+	 * Called when this library needs to allocate memory of the given size in bytes.
+	 * If this is NULL at the time swift_start is called, a default allocator will be used.
+	 */
+	swift_allocator_func_t allocator;
+	/**
+	 * Called when this library needs to re-allocate memory at the given pointer to be the given size.
+	 * If this is NULL at the time swift_start is called, a default re-allocator will be used.
+	 */
+	swift_reallocator_func_t reallocator;
+	/**
+	 * Called when this library needs to de-allocate memory at the given pointer.
+	 * If this is NULL at the time swift_start is called, a default de-allocator will be used.
+	 */
+	swift_deallocator_func_t deallocator;
+
+	/* This member (and its members, recursively) are 'private'. */
+	/* They should not be modified by your program unless you *really* know what you're doing. */
+	swift_context_private_t pvt;
+};
+
+typedef struct swift_context swift_context_t;
+
+/**
+ * Begin using this library.
+ * The context passed should be zeroed out, unless you wish to over-ride
+ * the function pointers within.
+ * Function pointers not zeroed out will be given meaningful defaults.
+ * This must be called early in the execution of your program,
+ * before additional threads (if any) are created.
+ * This must be called before any other use of this library by your program.
+ * These restrictions are imposed by libcurl, and the libcurl restrictions are in turn
+ * imposed by the libraries that libcurl uses.
+ * If your program is a library, it will need to expose a similar API to,
+ * and expose similar restrictions on, its users.
+ */
+enum swift_error swift_global_init(void);
+
+/**
+ * Cease using this library.
+ * This must be called late in the execution of your program,
+ * after all secondary threads (if any) have exited,
+ * so that there is precisely one thread in your program at the time of the call.
+ * This library must not be used by your program after this function is called.
+ * This function must be called exactly once for each successful prior call to swift_init
+ * by your program.
+ * These restrictions are imposed by libcurl, and the libcurl restrictions are in turn
+ * imposed by the libraries that libcurl uses.
+ * If your program is a library, it will need to expose a similar API to,
+ * and expose similar restrictions on, its users.
+ */
+void swift_global_cleanup(void);
+
+/**
+ * Begin using this library for a single thread of your program.
+ * This must be called by each thread of your program in order to use this library.
+ */
+enum swift_error swift_start(swift_context_t *context);
+
+/**
+ * Case using this library for a single thread.
+ * This must be called by each thread of your program after it is finished using this library.
+ * Each thread in your program must call this function precisely once for each successful call
+ * to swift_start by that thread.
+ * After this call, the context is invalid.
+ */
+void swift_end(swift_context_t **context);
+
+/**
+ * Retrieve an object from Swift.
+ */
+enum swift_error swift_get(swift_context_t *context);
+
+/**
+ * Insert or update an object in Swift.
+ */
+enum swift_error swift_put(swift_context_t *context);
+
+/**
+ * Insert or update metadata for an object.
+ */
+enum swift_error swift_post(swift_context_t *context, wchar_t *name, wchar_t *value);
+
+#endif /* SWIFT_CLIENT_H_ */
